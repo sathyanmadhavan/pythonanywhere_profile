e5774de3c8354494156f1fba57b9a4dd787ab00e
sathyanmadhavan <sathyanmadhavan619@gmail.com>
2023-01-20 14:40:42 +0530

    Updated

--- mybio.py
+++ mybio.py
@@ -9,7 +9,7 @@ app = flask(__name__)
 @app.route("/")
 def index_page():
     "The search page"
-    return render_template('resume.html')
+    return render_template('templates/resume.html')
 #----START OF SCRIPT
 if __name__=='__main__':
     app.run(host='0.0.0.0',port=6464)
