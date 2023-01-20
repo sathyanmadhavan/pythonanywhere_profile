bee5ee02cd446f01678cbeb636007152b31bd102
sathyanmadhavan <sathyanmadhavan619@gmail.com>
2023-01-20 22:34:09 +0530

    Updated

--- mybio.py
+++ mybio.py
@@ -9,28 +9,17 @@ from flask.wrappers import Response
 from github import Github
 
 app = Flask(__name__)
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    # Get the JSON data sent from GitHub
-    data = request.get_json()
-    # Create an instance of the Github client
-    g = Github(data['token'])
-    # Get the repository from the payload
-    repository = g.get_repo(data['pythonanywhere_profile']['sathyanmadhavan'])
-    url = 'https://www.pythonanywhere.com/webhook'
-   
-    # Do something with the commit object
-    # ...
-    return 'Success'
+
 
 @app.route('/git_update', methods=['POST'])
 def git_update():
-            repo = git.Repo('./pythonanywhere_profile')
-            origin = repo.remotes.origin
-            repo.create_head('main', 
-            origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
-            origin.pull()
-            return '', 200
+    repo = git.Repo('./orbe')
+    origin = repo.remotes.origin
+    repo.create_head('main',
+                     origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+    origin.pull()
+    return '', 200
+
 
 @app.route("/")
 def index_page():
