d9f8e2a49459c58677578da17a394803b5bef959
sathyanmadhavan <sathyanmadhavan619@gmail.com>
2023-01-20 20:42:28 +0530

    Updated file

--- mybio.py
+++ mybio.py
@@ -4,21 +4,22 @@ Habit: Develop -> test locally -> commit -> push to remote -> deploy to prod -> 
 
 """
 
-from flask import flask, render_template,request
+from flask import Flask, render_template,request
 import git
-app = flask(__name__)
+
+app = Flask(__name__)
 
 @app.route('/git_update', methods=['POST'])
 def git_update():
-    if request.method == 'POST':
-        repo = git.Repo('./pythonanywhere_profile')
-        origin = repo.remotes.origin
-        repo.create_head('main', 
+        if request.method == 'POST':
+            repo = git.Repo('./pythonanywhere_profile')
+            origin = repo.remotes.origin
+            repo.create_head('main', 
         origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
-        origin.pull()
-        return '', 200
-    else:
-        return '', 400
+            origin.pull()
+            return '', 200
+        else:
+            return '', 400
 
 @app.route("/")
 def index_page():
