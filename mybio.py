d4916b05f473403200458242ddce29204c0d32ea
sathyanmadhavan <sathyanmadhavan619@gmail.com>
2023-01-21 09:32:08 +0530

    Update

--- mybio.py
+++ mybio.py
@@ -10,20 +10,14 @@ import git
 app = Flask(__name__)
 
 
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    if request.method == 'POST':
-        # parse data from the request
-        data = request.get_json()
-        # use Git commands to pull code from remote repository
-        repo = git.Repo('./pythonanywhere_profile')
-        origin = repo.remotes.origin
-        repo.create_head('main', 
-        origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
-        origin.pull()
-        return '', 200
-    else:
-        return '', 400
+@app.route('/git_update', methods=['POST'])
+def git_update():
+    repo = git.Repo('./pythonanywhere_profile')
+    origin = repo.remotes.origin
+    repo.create_head('main',
+                     origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+    origin.pull()
+    return '', 200
 
 
 @app.route("/")
